1724cefcd5d3ea2d7804822a7e01c9adda574f60
Veronica Bustabad <vbustabad@gmail.com>
2018-06-30 16:13:44 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,6 +28,8 @@ def can_be_created_in_a_block(args = { title: "The Room", release_date: 2003 })
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
+    movie.title = "Home Alone"
+    movie.release_date = 1990
   end
 end
 
